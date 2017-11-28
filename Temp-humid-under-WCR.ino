3e1a48d9073d683585905cd410874617d45c0a4f
amitsharma-007 <34056932+amitsharma-007@users.noreply.github.com>
2017-11-28 17:09:39 +0530

    Add files via upload

---
+++ Temp-humid-under-WCR.ino
@@ -0,0 +1,40 @@
+
+
+/This is a subpart of a WCR for industrial 
+inspection project in TLC under the guidance of Dr. S. R. Pandian and is being done by Amit Sharma, M.Des EDS 2016 batch, roll no. 
+eds16m007.  /
+
+
+
+#include<dht.h>
+dht DHT;
+
+// if you require to change the pin number, Edit the pin with your arduino pin.
+
+#define DHT11_PIN 3
+
+void setup() {
+
+Serial.begin(9600);
+
+ }
+
+void loop() { // READ DATA
+
+Serial.println("Humidity and Temperature of the Field Area");
+
+Serial.println(" ");
+
+int chk = DHT.read11(DHT11_PIN);
+
+Serial.print(" Humidity       : " );
+
+Serial.println(DHT.humidity, 1);
+
+Serial.print(" Temparature    : ");
+
+Serial.println(DHT.temperature, 1);
+
+delay(2000);
+
+}
